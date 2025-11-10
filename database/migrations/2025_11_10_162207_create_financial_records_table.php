49572ecc50d694e80b98850b79ff684527b80be8
Daniel Alvero <dalvero589@gmail.com>
2025-11-10 23:33:47 +0700

    setup and migration

---
+++ database/migrations/2025_11_10_162207_create_financial_records_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_records', function (Blueprint $table) {
+            $table->id();
+            $table->enum('tipe', ['income', 'expense']);
+            $table->string('deskripsi')->nullable();
+            $table->decimal('jumlah', 12, 2);
+            $table->dateTime('tanggal');
+            $table->foreignId('shift_id')->nullable()->constrained('shift_sessions')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_records');
+    }
+};
